4bddd0934278847ef2fc72b5ef16435108e78c26
kristoffabjan <kristof.fabjan@gmail.com>
2021-02-19 19:00:39 +0100

    basic setup and added bike table

--- test/routes/web.php
+++ test/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BikeController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/app');
 });
+
+Route::get('/index', function () {
+    return view('layouts/index');
+});
+
+Route::get('/bikes', [BikeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index']);
